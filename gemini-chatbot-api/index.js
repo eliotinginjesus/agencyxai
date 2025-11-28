93bc191b7e7c45eb3acf16480129e940c9364c95
eliotinginjesus <88655734+eliotinginjesus@users.noreply.github.com>
2025-11-29 00:04:42 +0700

    last

--- gemini-chatbot-api/index.js
+++ gemini-chatbot-api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const dotenv = require('dotenv');
+require('dotenv').config(); // <-- PERBAIKAN: Memuat variabel dari .env
 const cors = require('cors');
 const {
   GoogleGenerativeAI,
